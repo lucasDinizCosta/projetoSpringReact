26d0e448709efe35e300a0016046335180797e19
lucasDinizCosta <lucasdinizcostaprofessional@gmail.com>
2022-01-10 16:24:58 -0300

    MovieCard

--- dsmovie/frontend/src/pages/Listing/index.tsx
+++ dsmovie/frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,35 @@
 import Pagination from "components/Pagination";
+import MovieCard from "components/MovieCard";
 
 function Listing(){
     return (
-        <Pagination />
+        // Por padrão o react só aceita que retorne 1 elemento, para contornar isso pode se
+        // adicionar os elementos em uma div, contudo, ocupará espaço no HTML
+        // ou em uma tag vazia <>ELEMENTOS AQUI</>
+        <>
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                </div>
+                
+            </div>
+        </>
     );
 }
 
