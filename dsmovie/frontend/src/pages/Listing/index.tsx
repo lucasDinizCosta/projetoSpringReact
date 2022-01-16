bf2bf8dc546c2ed50fe2523104bb9dda19408313
lucasDinizCosta <lucasdinizcostaprofessional@gmail.com>
2022-01-16 15:31:40 -0300

    Props

--- dsmovie/frontend/src/pages/Listing/index.tsx
+++ dsmovie/frontend/src/pages/Listing/index.tsx
@@ -7,20 +7,31 @@ import { BASE_URL } from "utils/request";
 
 function Listing(){
 
-    // definindo a hook do useState
+    // definindo a hook dos estados através do useState
     const [pageNumber, setPageNumber] = useState(0);
+
+    // Estado do tipo 'MoviePage'
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+    });
 
     // definindo a hook do useEffect
     // ==> Executa somente quando o componente é totalmente carregado
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        axios.get(`${BASE_URL}/movies?size=${page.size}&page=${pageNumber}`)
         .then(response => {
             const data = response.data as MoviePage;
-            setPageNumber(data.number)
-            console.log(data);
-            
+            setPage(data);
         });
-    }, [])
+    }, [pageNumber])        // Quando mudar o pageNumber refará uma nova requisição
 
     // Forma ERRADA(SEM USAR HOOKS)===> Vem 2 requisições
     // axios.get(`${BASE_URL}/movies?size=12&page=0`)
@@ -30,33 +41,33 @@ function Listing(){
     //     });
 
 
+    // const movie = {
+    //     id: 1,
+    //     image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+    //     title: "The Witcher",
+    //     count: 2,
+    //     score: 4.5
+    // };
+
     return (
         // Por padrão o react só aceita que retorne 1 elemento, para contornar isso pode se
         // adicionar os elementos em uma div, contudo, ocupará espaço no HTML
         // ou em uma tag vazia <>ELEMENTOS AQUI</>
         <>
-            <p>{pageNumber}</p>
             <Pagination />
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-
+                    {
+                        // NOTA: em uma renderização dinâmica de coleção, 
+                        // cada elemento renderizado DEVE possuir um atributo 'key'.
+                        page.content.map(movie => (
+                            <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                                <MovieCard movie={movie} />
+                            </div>
+                        ))
+                    }
+                    
                 </div>
-                
             </div>
         </>
     );
